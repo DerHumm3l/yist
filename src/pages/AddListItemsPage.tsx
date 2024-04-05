9b996d777d4cca6f85697d9099e99bb225cd5b9e
Hummel <lukas.maibaum@tutanota.com>
2024-04-05 23:14:49 +0200

    add minor AddListItemPage changes

--- src/pages/AddListItemsPage.tsx
+++ src/pages/AddListItemsPage.tsx
@@ -1,21 +1,42 @@
 import LinkButton from "@/components/LinkButton";
+import { Button } from "@/components/ui/button";
 import { listSelector, useListsStore } from "@/lib/store";
 import { createList } from "@/lib/utils";
-import { useParams } from "react-router-dom";
+import { List } from "@/types/list";
+import { ListItem } from "@/types/listItem";
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function AddListItemsPage() {
   const { id } = useParams();
-  const { list, addList } = useListsStore(listSelector(id));
+  const navigate = useNavigate();
+
+  const { list, addList, addListItems, deleteList } = useListsStore(
+    listSelector(id)
+  );
+
+  const [listItems, setListItems] = React.useState([] as ListItem[]);
 
   if (!list && id) {
     // create list
     addList(createList(id));
   }
 
+  const cancel = () => {
+    // navigate back
+    // delete list if new
+    navigate(-1);
+  };
+
+  const submit = () => {
+    // save listItems
+  };
+
   return (
     <main className="">
       <h1>Add list items page</h1>
-      <LinkButton to={`/list/${id}`}>Submit</LinkButton>
+      <Button onClick={cancel}>Cancel</Button>
+      <Button onClick={submit}>Submit</Button>
     </main>
   );
 }
