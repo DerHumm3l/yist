6475f9f6dddb0d8ad40cdd3f2ddbbbc9d8da3bf4
Hummel <lukas.maibaum@tutanota.com>
2024-04-12 19:00:31 +0200

    clean storage

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,43 +1,25 @@
-import { List } from "@/types/list";
 import { Preferences } from "@capacitor/preferences";
 import { StateStorage } from "zustand/middleware";
 
-const LIST_STORAGE_KEY = "lists";
-const LIST_ITEMS_STORAGE_KEY = "listItems";
-
 export const storage: StateStorage = {
-  getItem: async (key) => {
-    const result = await Preferences.get({ key });
-    return result.value;
-  },
-  setItem: (key, value) => Preferences.set({ key, value }),
-  removeItem: (key) => Preferences.remove({ key }),
+  getItem,
+  setItem,
+  removeItem,
 };
 
-export async function clear() {
+export function clear() {
   return Preferences.clear();
 }
 
-export async function getLists() {
-  const lists = await Preferences.get({ key: LIST_STORAGE_KEY });
-  return lists.value ? JSON.parse(lists.value) : [];
+async function getItem(key: string) {
+  const result = await Preferences.get({ key });
+  return result.value;
 }
 
-export function saveLists(lists: List[]) {
-  return Preferences.set({
-    key: LIST_STORAGE_KEY,
-    value: JSON.stringify(lists),
-  });
+function setItem(key: string, value: string) {
+  return Preferences.set({ key, value });
 }
 
-export async function getListItems() {
-  const listItems = await Preferences.get({ key: LIST_ITEMS_STORAGE_KEY });
-  return listItems.value ? JSON.parse(listItems.value) : [];
-}
-
-export function saveListItems(listItems: List[]) {
-  return Preferences.set({
-    key: LIST_ITEMS_STORAGE_KEY,
-    value: JSON.stringify(listItems),
-  });
+function removeItem(key: string) {
+  return Preferences.remove({ key });
 }
