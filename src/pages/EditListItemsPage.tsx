46988df87cbf9b7a29d692638693f6e0017eb2f2
Hummel <lukas.maibaum@tutanota.com>
2024-04-18 19:57:08 +0200

    implement list items in editListItemsPage

--- src/pages/EditListItemsPage.tsx
+++ src/pages/EditListItemsPage.tsx
@@ -1,7 +1,18 @@
+import { listItemsSelector, useListsStore } from "@/lib/store";
+import { useParams } from "react-router-dom";
+
 export default function EditListItemsPage() {
+  const { id: listId } = useParams();
+  const { listItems } = useListsStore(listItemsSelector(listId));
+
   return (
     <main className="">
       <h1>Edit list items page</h1>
+      <ul>
+        {listItems.map((listItem) => (
+          <li key={listItem.id}>{listItem.name}</li>
+        ))}
+      </ul>
     </main>
   );
 }
