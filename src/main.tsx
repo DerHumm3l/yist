61ef176c91c7440e284d71d86d36671edae7c55c
Hummel <lukas.maibaum@tutanota.com>
2024-05-09 10:32:19 +0200

    improve back navigation

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,36 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { App } from "@capacitor/app";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+async function addBackButtonListener() {
+  return App.addListener("backButton", (e) => {
+    e.canGoBack = false;
+
+    const pathname = window.location.pathname;
+
+    if (pathname === "/") {
+      App.exitApp();
+    } else {
+      window.history.back();
+    }
+  });
+}
+
+function Yist() {
+  React.useEffect(() => {
+    addBackButtonListener();
+
+    return () => {
+      App.removeAllListeners();
+    };
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<Yist />);
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,27 +3,9 @@ import AddListButton from "@/components/addListButton";
 import * as Header from "@/components/header";
 import SettingsButton from "@/components/settingsButton";
 import { useLists } from "@/lib/state/hooks";
-import { useNavigate } from "react-router-dom";
-import { App } from "@capacitor/app";
-import React from "react";
 
 export default function HomePage() {
   const lists = useLists();
-  const navigate = useNavigate();
-
-  React.useEffect(() => {
-    App.addListener("backButton", (e) => {
-      e.canGoBack = false;
-
-      const pathname = window.location.pathname;
-
-      if (pathname === "/") {
-        App.exitApp();
-      } else {
-        navigate(-1);
-      }
-    });
-  }, []);
 
   return (
     <main className="flex flex-col items-center m-auto max-w-screen-md p-4">
